daf2ad4b8b1702366f3f4e210e3c3e572435ca60
carlosemh871208 <32914128+carlosemh871208@users.noreply.github.com>
2017-11-13 11:34:36 -0600

    Defintions of new types in the library called definitions.h

---
+++ Scheduler/include/MCAL/definitions.h
@@ -0,0 +1,66 @@
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: defintions.h $
+ * $Revision: 1 $
+ * $Author: Carlos Martinez $
+ * $Date: 13/11/2017 $
+ */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \definitions.h
+
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*        AUTHOR       |       VERSION      |           DESCRIPTION           */
+/*----------------------------------------------------------------------------*/
+/*   Carlos Martinez   |         1.1        | All types definition            */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*
+ * $Log: filename.h  $
+  ============================================================================*/
+#ifndef MCAL_DEFINITIONS_H_
+#define MCAL_DEFINITIONS_H_
+
+/* Includes */
+/*============================================================================*/
+
+
+/* Constants and types */
+/*============================================================================*/
+#define NULL (void*) 0ul
+
+typedef unsigned char T_UBYTE;
+typedef signed char T_SBYTE;
+typedef unsigned short T_UWORD;
+typedef signed short T_SWORD;
+typedef unsigned long T_ULONG;
+typedef signed long T_SLONG;
+
+/* Exported Variables */
+/*============================================================================*/
+
+
+/* Exported functions prototypes */
+/*============================================================================*/
+
+#endif  /* Notice: the file ends with a blank new line to avoid compiler warnings */
